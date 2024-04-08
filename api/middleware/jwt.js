cc5fd09e42db92e36e03148898d5e84361090cf4
Suvranil G <suvranilghose4@gmail.com>
2024-04-09 01:34:15 +0530

    Updated auth token source

--- api/middleware/jwt.js
+++ api/middleware/jwt.js
@@ -2,8 +2,8 @@ import jwt from "jsonwebtoken";
 import createError from "../utils/createError.js";
 
 export const verifyToken = (req, res, next) => {
-  // const token = req.cookies.accessToken;
-  const token=req.headers['X-AccessToken'];
+  const token = req.cookies.accessToken;
+  // const token=req.headers['X-AccessToken'];
   console.log(token);
   if (!token) return next(createError(401,"You are not authenticated!"))
 
