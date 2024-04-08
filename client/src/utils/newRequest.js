3344440b907ad51d4acbc20b0b00ff9463595c49
Suvranil G <suvranilghose4@gmail.com>
2024-04-08 19:15:47 +0530

    Disabled withCredentials

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -4,7 +4,7 @@ const newRequest = axios.create({
   // baseURL: "http://localhost:8800/api/",
   // baseURL: "https://free-lancer-api.vercel.app/api/",
   baseURL: "https://freelancerwebapp.onrender.com/api/",
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 export default newRequest;
