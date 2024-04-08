25638aaa3094c251aa9f94d0148d4d0874e13026
Suvranil G <suvranilghose4@gmail.com>
2024-04-08 21:32:22 +0530

    Updated Cors origin

--- api/routes/order-route.js
+++ api/routes/order-route.js
@@ -5,7 +5,7 @@ import { getOrders, intent, confirm } from "../controllers/order-controller.js";
 const router = express.Router();
 
 // router.post("/:gigId", verifyToken, createOrder);
-router.get("/", verifyToken, getOrders);
+router.get("/", verifyToken,getOrders);//, verifyToken
 router.post("/create-payment-intent/:id", verifyToken, intent);
 router.put("/", verifyToken, confirm);
 
--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  // baseURL: "http://localhost:8800/api/",
+  baseURL: "http://localhost:8800/api/",
   // baseURL: "https://free-lancer-api.vercel.app/api/",
-  baseURL: "https://freelancerwebapp.onrender.com/api/",
+  // baseURL: "https://freelancerwebapp.onrender.com/api/",
   withCredentials: true,
 });
 
