064c8dbb72a8c62eff10c3f6999e59e65ecbe41a
Suvranil G <suvranilghose4@gmail.com>
2024-04-08 13:23:48 +0530

    Updated route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -6,5 +6,8 @@ const router = express.Router();
 
 router.delete("/:id", verifyToken, deleteUser);
 router.get("/:id", getUser);
+router.get("/index", (req, res) => {
+    res.send("Hello Freelancers");
+  });
 
 export default router;
